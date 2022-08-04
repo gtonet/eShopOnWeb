e2cc4e9342738dd87754872dbdee437819570690
G sz <40786276+gtonet@users.noreply.github.com1>
2022-08-04 15:02:46 +0800

    For module: Service Bus, Event Hub, Event Grid

--- src/OrderItemsReserverFunction/OrderItemsReserver.cs
+++ src/OrderItemsReserverFunction/OrderItemsReserver.cs
@@ -8,18 +8,24 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
+using Microsoft.Azure.WebJobs.Host;
+using System.Net.Http;
+using Microsoft.Extensions.Configuration;
+using System.Text;
 
 namespace OrderItemsReserverFunction
 {
     public class OrderItemsReserver
     {
         [FunctionName("OrderItemsReserver")]
+        [FixedDelayRetry(3, "00:00:10")]
         public async Task<string> Run(
+            ExecutionContext context,
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
              [Blob("basket", FileAccess.ReadWrite, Connection = "StorageConnectionString")] Azure.Storage.Blobs.BlobContainerClient blobContainer,
             ILogger log)
         {
-            log.LogInformation("Received a basket.");
+            log.LogInformation("Received order info.");
             string responseMessage = String.Empty;
             var body = await new StreamReader(req.Body).ReadToEndAsync();
             try
@@ -32,14 +38,48 @@ namespace OrderItemsReserverFunction
                 BinaryData data = new BinaryData(body);
                 await cloudBlockBlob.UploadAsync(data);
 
-                responseMessage = $"The basket {newId} is upload successfully.";
+
+                responseMessage = $"The order {newId} is upload successfully.";
             }
             catch (Exception exp)
             {
                 responseMessage = $"Exception: {exp.Message}";
+
+                var currentRetryCount = context.RetryContext.RetryCount;
+                if (currentRetryCount >= context.RetryContext.MaxRetryCount)
+                {
+                    var config = new ConfigurationBuilder()
+                        .SetBasePath(context.FunctionAppDirectory)
+                        .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                        .AddEnvironmentVariables()
+                        .Build();
+
+                    var logicAppUrl = config["LOGIC_APP_URL"];
+                }
+                else
+                {
+                    throw exp;
+                }
             }
 
             return responseMessage;
+        }
+
+        private async Task SendEmail(string logicAppUrl)
+        {
+            var client = new HttpClient();
+            var jsonData = JsonConvert.SerializeObject(new
+            {
+                email = "<name>@outlook.com",
+                due = System.DateTime.Now,
+                task = "Save order data failed."
+            });
+
+            HttpResponseMessage result = await client.PostAsync(
+                logicAppUrl,
+                new StringContent(jsonData, Encoding.UTF8, "application/json"));
+
+            var statusCode = result.StatusCode.ToString();
         }
     }
 }
--- src/OrderItemsReserverFunction/SaveOrder.cs
+++ src/OrderItemsReserverFunction/SaveOrder.cs
@@ -9,14 +9,84 @@ using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 using Microsoft.eShopWeb.ApplicationCore.Entities.BasketAggregate;
 using Microsoft.eShopWeb.ApplicationCore.Entities.OrderAggregate;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http;
+using System.Text;
 
 namespace OrderItemsReserverFunction
 {
     public class SaveOrder
     {
-       
+
         [FunctionName("SaveOrder")]
+        [FixedDelayRetry(3, "00:00:10")]
         public async Task<string> Run(
+            ExecutionContext context,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+             [Blob("orders", FileAccess.ReadWrite, Connection = "StorageConnectionString")] Azure.Storage.Blobs.BlobContainerClient blobContainer,
+            ILogger log)
+        {
+            log.LogInformation("Received order info.");
+            string responseMessage = String.Empty;
+            var body = await new StreamReader(req.Body).ReadToEndAsync();
+            try
+            {
+                var newId = Guid.NewGuid();
+                var blobName = $"{newId}.json";
+
+                await blobContainer.CreateIfNotExistsAsync();
+                var cloudBlockBlob = blobContainer.GetBlobClient(blobName);
+                BinaryData data = new BinaryData(body);
+                await cloudBlockBlob.UploadAsync(data);
+
+
+                responseMessage = $"The order {newId} is upload successfully.";
+            }
+            catch (Exception exp)
+            {
+                responseMessage = $"Exception: {exp.Message}";
+
+                var currentRetryCount = context.RetryContext.RetryCount;
+                if (currentRetryCount >= context.RetryContext.MaxRetryCount)
+                {
+                    var config = new ConfigurationBuilder()
+                        .SetBasePath(context.FunctionAppDirectory)
+                        .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                        .AddEnvironmentVariables()
+                        .Build();
+
+                    var logicAppUrl = config["LOGIC_APP_URL"];
+                    await SendEmail(logicAppUrl);
+                }
+                else
+                {
+                    throw exp;
+                }
+            }
+
+            return responseMessage;
+        }
+
+        private async Task SendEmail(string logicAppUrl)
+        {
+
+            var client = new HttpClient();
+            var jsonData = JsonConvert.SerializeObject(new
+            {
+                email = "<name>@outlook.com",
+                due = System.DateTime.Now,
+                task = "Save order data failed."
+            });
+
+            HttpResponseMessage result = await client.PostAsync(
+                logicAppUrl,
+                new StringContent(jsonData, Encoding.UTF8, "application/json"));
+
+            var statusCode = result.StatusCode.ToString();
+        }
+
+        [FunctionName("SaveOrder01")]
+        public async Task<string> Run01(
             [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
              [CosmosDB("eShop1",
                       "OrderInfo",
